d069332bdb3185c5be48c88ad5f5303b75f2d569
liyuze <409654850@qq.com>
2017-11-22 01:27:50 +0800

    主界面搭建

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  isCollapsed = true;
+
+  constructor(){}
+
+  log(msg: string){
+    console.log(msg);
+  }
 }
